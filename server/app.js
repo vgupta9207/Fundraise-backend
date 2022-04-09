48d9f90eb6f5aceb0606533a99a810ebd5b24475
vgupta9207 <91655979+vgupta9207@users.noreply.github.com>
2022-04-09 02:30:20 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,14 +12,14 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../front/public')))
+// app.use(express.static(path.join(__dirname, '../front/public')))
 // routes
 app.use("/users", usersRoutes);
 app.use("/transactions", transactionsRoutes);
 app.use("/fundraises", fundraisesRoutes);
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../front/public/index.html'))
-  })
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/../front/public/index.html'))
+//   })
 
 console.log(process.env.DB_CONNECTION);
 // Connect to DB
